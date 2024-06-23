82919f554c4f71da80b51962e1f41bee5feb8a23
Michael Mark <michael.mark@oit.edu>
2024-06-22 23:30:12 -0700

    Made things prettier (UI + structure)

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function App() {
     return (<>
-        <h1>Hello, world!</h1>
-        <p>Soon to become some fancy Gleam docs!</p>
+        <nav className="bg-green-200 fixed top-0 w-full">
+            <h1 className="text-3xl text-black p-1">Michael Mark's Blog</h1>
+        </nav>
+        <div className="h-11"></div>
+        <p>Gleam, when it's completely by itself, kinda sucks. That's why we have the `@external` attribute!</p>
     </>)
 }
